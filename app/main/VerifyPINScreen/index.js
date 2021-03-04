ebd3625066a2f5209035e8c656e1dc931c23c55c
Nguyen Hong Phuc <PhucNHb@bkav.com>
2021-03-04 22:35:54 +0700

    Day code them faceidy

--- app/main/VerifyPINScreen/index.js
+++ app/main/VerifyPINScreen/index.js
@@ -20,8 +20,10 @@ import {
   SafeAreaView,
   KeyboardAvoidingView,
   View,
+  Alert,
 } from 'react-native';
 import {Button} from 'react-native-elements';
+import TouchID from 'react-native-touch-id';
 
 // Styles
 import styles from './styles/index.css';
@@ -36,6 +38,18 @@ import global, {
 } from '../../global';
 import {setPinCode, getPinCode} from '../../core/storage';
 
+const optionalConfigObject = {
+  title: 'Authentication Required', // Android
+  imageColor: '#e00606', // Android
+  imageErrorColor: '#ff0000', // Android
+  sensorDescription: 'Touch sensor', // Android
+  sensorErrorDescription: 'Failed', // Android
+  cancelText: 'Cancel', // Android
+  fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
+  unifiedErrors: false, // use unified error messages (default false)
+  passcodeFallback: true, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
+};
+
 class VerifyPINScreen extends PureComponent {
   constructor(props) {
     super(props);
@@ -47,7 +61,7 @@ class VerifyPINScreen extends PureComponent {
     this.onFirstVerify = false;
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const {membercode, token} = global;
     getAccountBalanceGlobal();
 
@@ -60,6 +74,32 @@ class VerifyPINScreen extends PureComponent {
         getUserGlobal(membercode);
       },
     );
+
+    TouchID.isSupported(optionalConfigObject)
+      .then((biometryType) => {
+        debugger;
+        // Success code
+        if (biometryType === 'FaceID') {
+          TouchID.authenticate(
+            'to demo this react-native component',
+            optionalConfigObject,
+          )
+            .then((success) => {
+              alert('Authenticated Successfully');
+            })
+            .catch((error) => {
+              alert('Authentication Failed');
+            });
+          console.log('FaceID is supported.');
+        } else {
+          console.log('TouchID is supported.');
+        }
+      })
+      .catch((error) => {
+        // Failure code
+        debugger;
+        console.log('TouchID', error);
+      });
   }
 
   pinInput = React.createRef();
@@ -135,19 +175,13 @@ class VerifyPINScreen extends PureComponent {
                 }
                 style={styles.text}
               />
-              {pinCodeActive ? (
-                <Button
-                  title="Xác thực mã PIN"
-                  buttonStyle={styles.btnButtonStyle}
-                  onPress={this.onVerifyOTP}
-                />
-              ) : (
-                <Button
-                  title="Nhập mã PIN"
-                  buttonStyle={styles.btnButtonStyle}
-                  onPress={this.onVerifyOTP}
-                />
-              )}
+              <Button
+                title={pinCodeActive ? 'Xác thực mã PIN' : 'Nhập mã PIN'}
+                buttonStyle={
+                  pinCodeActive ? styles.btnButtonStyle : styles.btnButtonStyle
+                }
+                onPress={this.onVerifyOTP}
+              />
             </View>
           </KeyboardAvoidingView>
         </SafeAreaView>
